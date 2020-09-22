f4d2b9875097f41bc74157f6d944eef9701a8e04
Satyam-Anand-i8Labs <satyam@i8labs.io>
2020-09-22 17:31:23 +0530

    Updates

---
+++ precache-manifest.b2c8ca5e50587021642b4a98c382c228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a3ad47160364aa2cbe951e5355b5b6",
+    "url": "/IndyWiseReactApp/index.html"
+  },
+  {
+    "revision": "0678df754dc74a36f086",
+    "url": "/IndyWiseReactApp/static/js/2.603304f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/IndyWiseReactApp/static/js/2.603304f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "674544994961de591559",
+    "url": "/IndyWiseReactApp/static/js/main.338e1511.chunk.js"
+  },
+  {
+    "revision": "37685b321ec9b4465d2e",
+    "url": "/IndyWiseReactApp/static/js/runtime-main.5c43dbe6.js"
+  }
+]);
